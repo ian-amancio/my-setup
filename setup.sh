e8919fa05f99842be187329cb7090434f9923372
Ian Amancio <ianamancio@proton.me>
2025-05-16 10:03:09 -0300

    Add: Awesome

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+HOME="/home/ian"
+
+AWESOME_SRC="$HOME/projects/my-setup/awesome"
+AWESOME_DIR="$HOME/.config/awesome"
+
+echo "RUN - $(date)"
+
+echo "[Awesome]"
+
+if [ -e "$AWESOME_DIR" ]; then
+	if [ -L "$AWESOME_DIR" ]; then
+		echo "- Ok"
+	else
+		echo "- Create folder backup and symlink"
+		mv "$AWESOME_DIR" "$HOME/.config/awesome.backup"
+		ln -s "$AWESOME_SRC" "$AWESOME_DIR"
+	fi
+else
+	echo "- Creating symlink"
+	ln -s "$AWESOME_SRC" "$AWESOME_DIR"
+fi
